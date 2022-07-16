45de55911ccf8735778697b2b9c8b5096e06e317
Dawesome101 <dawesome@danawesome.com>
2022-07-16 11:38:10 -0700

    Solved for function 5

--- js/app.js
+++ js/app.js
@@ -83,6 +83,7 @@ function sumArray(sumArr) { //eslint-disable-line
   return [sum1, str1];
 }
 
+
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
@@ -100,12 +101,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
+//multiplyArray(testArray);
+
+function multiplyArray(multArr) { //eslint-disable-line
+  let prod1 = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+  let str1 = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + prod1 + '.';
+  return [prod1, str1];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+
+//testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
